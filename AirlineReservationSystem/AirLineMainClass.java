a93675439e95f3e411c0a8ff0c6ad6de2fa750f0
Patrick OJunde <patrickojunde@gmail.com>
2020-02-08 16:58:30 +0100

    A (mini) Airline Reservation System

---
+++ AirlineReservationSystem/AirLineMainClass.java
@@ -0,0 +1,57 @@
+package AirlineReservationSystem;
+
+public class AirLineMainClass {
+
+    public static void main(String... args){
+
+        AirlineReservationSystem user = new AirlineReservationSystem();
+        AirlineReservationSystem user2 = new AirlineReservationSystem();
+        AirlineReservationSystem user3 = new AirlineReservationSystem();
+        AirlineReservationSystem user4 = new AirlineReservationSystem();
+        AirlineReservationSystem user5 = new AirlineReservationSystem();
+        AirlineReservationSystem user6 = new AirlineReservationSystem();
+        AirlineReservationSystem user7 = new AirlineReservationSystem();
+        AirlineReservationSystem user8 = new AirlineReservationSystem();
+        AirlineReservationSystem user9 = new AirlineReservationSystem();
+        AirlineReservationSystem user10 = new AirlineReservationSystem();
+        AirlineReservationSystem user11 = new AirlineReservationSystem();
+//user.DisplayBoardingPass();
+
+        //1
+        AirlineReservationSystem.setClass();  user.getSeatAvailability();
+
+       //2
+        AirlineReservationSystem.setClass();  user2.getSeatAvailability();
+
+        //3
+        AirlineReservationSystem.setClass();  user3.getSeatAvailability();
+
+        //4
+        AirlineReservationSystem.setClass();  user4.getSeatAvailability();
+
+        //5
+        AirlineReservationSystem.setClass();  user5.getSeatAvailability();
+
+        //6
+        AirlineReservationSystem.setClass();  user6.getSeatAvailability();
+
+        //7
+        AirlineReservationSystem.setClass();  user7.getSeatAvailability();
+
+        //8
+        AirlineReservationSystem.setClass(); user8.getSeatAvailability();
+
+        //9
+        AirlineReservationSystem.setClass();  user9.getSeatAvailability();
+
+        //10
+        AirlineReservationSystem.setClass();  user10.getSeatAvailability();
+
+        //11
+        AirlineReservationSystem.setClass();  user11.getSeatAvailability();
+
+
+
+
+    }
+}
---
+++ AirlineReservationSystem/AirlineReservationSystem.java
@@ -0,0 +1,4 @@
+package AirlineReservationSystem;
+
+public class AirlineReservationSystem {
+}
